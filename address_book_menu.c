16c71a50f4804bee51f82fcf15eff0808a416617
Loosineh Hartoonian <98439771+loosineh@users.noreply.github.com>
2022-03-23 16:20:42 -0700

    Update address_book_menu.c

--- address_book_menu.c
+++ address_book_menu.c
@@ -133,7 +133,8 @@ Status menu(AddressBook *address_book)
 
 Status add_contacts(AddressBook *address_book)
 {
-   char choice;
+    char choice;
+    char select[10];
     ContactInfo* contactInfo = address_book -> list; 
     contactInfo = (ContactInfo*) realloc(contactInfo, ((address_book -> count + 1) * sizeof(contactInfo)));
 
