9db41d0f6184ea5e456603283881d047e15c92cb
shane green <shanegreen@cpp.edu>
2022-03-27 12:11:15 -0700

    add method, needs formatting

--- address_book_menu.c
+++ address_book_menu.c
@@ -19,6 +19,7 @@ int get_option(int type, const char *msg)
      */
 
     /* Fill the code to add above functionality */
+    return 0;
 }
 
 Status save_prompt(AddressBook *address_book)
@@ -133,25 +134,108 @@ Status menu(AddressBook *address_book)
 
 Status add_contacts(AddressBook *address_book)
 {
-    /* Add the functionality for adding contacts here */
+    //check if addressbook is NULL
+    if (address_book == NULL)
+        return e_fail;
+    if (address_book->list == NULL) 
+        return e_fail;
+
+    //reallocate contact list with 1 more on size
+    address_book->count += 1;
+    address_book->list = realloc(address_book->list, sizeof(ContactInfo) * address_book->count);
+
+    //take user input for name, nums, emails
+    char nameInput[NAME_LEN];
+    char numberInput[PHONE_NUMBER_COUNT][NUMBER_LEN];
+    char emailInput[EMAIL_ID_COUNT][EMAIL_ID_LEN];
+    int nameCount = 0;
+    int numberCount = 0;
+    int emailCount = 0;
+    int choice = -1;
+    int quit = 0;
+    do {
+        printf("\nto go back enter 0\nto input name enter 1\nto input number #%d enter 2\nto input email #%d enter 3\n", numberCount+1, emailCount+1);
+        scanf(" %d", &choice);
+        switch (choice) {
+            case 0:
+                printf("quitting\n");
+                quit = 1;
+                break;
+            case 1:
+                if (nameCount > 0) {
+                    printf("max number of names already entered\n");
+                    break;
+                } else {
+                    printf("adding name\n");
+                    scanf(" %[^\n]%*c", nameInput);
+                    strcpy(((address_book->list)+(address_book->count - 1))->name[0], nameInput);
+                    printf("entered name: %s\n", ((address_book->list)+(address_book->count - 1))->name[0]);
+                    nameCount++;
+                }
+                break;
+            case 2:
+                if (numberCount > 4) {
+                    printf("max number of phone numbers already entered\n");
+                    break;
+                } else {
+                    printf("adding number #%d", numberCount+1);
+                    scanf(" %s", numberInput[numberCount]);
+                    strcpy(((address_book->list)+(address_book->count - 1))->phone_numbers[numberCount], numberInput[numberCount]);
+                    printf("entered number: %s\n", ((address_book->list)+(address_book->count - 1))->phone_numbers[numberCount]);
+                    numberCount++;
+                }
+                break;
+            case 3:
+                if (emailCount > 4) {
+                    printf("max number of emails already entered\n");
+                } else {
+                    printf("adding email #%d", emailCount+1);
+                    scanf(" %s", emailInput[emailCount]);
+                    strcpy(((address_book->list)+(address_book->count - 1))->email_addresses[emailCount], emailInput[emailCount]);
+                    printf("entered email: %s\n", ((address_book->list)+(address_book->count - 1))->email_addresses[emailCount]);
+                    emailCount++;
+                }
+                break;
+            default:
+                printf("error making choice, %d", choice);
+                break;
+
+        }
+        printf("\n");
+    } while (quit == 0);
+    //setting serial number
+    ((address_book->list) + (address_book->count - 1))->si_no = address_book->count;
+    
+    return e_success;
 }
 
 Status search(const char *str, AddressBook *address_book, int loop_count, int field, const char *msg, Modes mode)
 {
     /* Add the functionality for adding contacts here */
+    return e_success;
 }
 
 Status search_contact(AddressBook *address_book)
 {
     /* Add the functionality for search contacts here */
+    return e_success;
 }
 
 Status edit_contact(AddressBook *address_book)
 {
     /* Add the functionality for edit contacts here */
+    return e_success;
 }
 
 Status delete_contact(AddressBook *address_book)
 {
     /* Add the functionality for delete contacts here */
+    return e_success;
+}
+
+int main() {
+    AddressBook address_book;
+    load_file(&address_book);
+    add_contacts(&address_book);
+    save_file(&address_book);
 }
