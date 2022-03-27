4359a8c4c25b755699585f9e32265102979fc198
Abigail Pinkus <arpinkus@cpp.edu>
2022-03-27 12:30:29 -0700

    Trying to find bugs in search.c

--- search.c
+++ search.c
@@ -1,45 +1,51 @@
+#include <stdio.h>
+//#include <stdio_ext.h>
+#include <stdlib.h>
 #include <string.h>
-#include <stdio.h> // input/output
-#include <stdlib.h>  //libraries
 #include <ctype.h>
-//#include "address_book_fops.h" 
+
+#include "address_book_fops.h"
+//#include "abk_log.h"
 #include "address_book_menu.h"
 #include "address_book.h"
 
 void main(AddressBook* address_book) {
+    printf("hello world\n");
     search_contact(address_book);
 }
 
 Status search_contact(AddressBook* address_book) {
+    printf("hello world inside\n");
     AddressBook * addressBook = address_book; 
-    ContactInfo * contactInfo = address_book -> list;
     ContactInfo *contactPtr = address_book ->list;
+    printf("world hello\n");
     int count = addressBook->count;
     const char targetName[255];
     const char targetPhone[255];
     const char targetEmail[255];
-    int targetSerial;
+    int targetSerial = 0;
     char choice = '0';
     int i = 0;
     
 
     /* Select how to search the specific contact by name, by phone number, email id, or by serial number */
-    SELECT_SEARCH:
-    printf("Select Contact to search by: \n"
-    "0. Back\n"
-    "1. Name\n"
-    "2. Phone No\n"
-    "3. Email ID\n"
-    "4. Serial No\n"
-    "Please select an option number:\n"); 
-    scanf("%s", &choice);
+    /*// printf("Select Contact to search by: \n");
+    // printf("0. Back\n");
+    // printf("1. Name\n");
+    // printf("2. Phone No\n");
+    // printf("3. Email ID\n");
+    // printf("4. Serial No\n");
+    // printf("Please select an option number:\n"); 
+    // scanf("%s", &choice);*/
+
+    printf("hollow world\n");
 
     switch(choice) {
-        case 0:
-            printf("Back\n");
-            return e_back;
-            break;
-        case 1:
+        case '0':
+        printf("Back\n");
+        return e_back;
+        break;
+        case '1':
             printf("What name would you like to search for:\n");
             scanf("%s\n", &targetName);
 
@@ -52,7 +58,7 @@ Status search_contact(AddressBook* address_book) {
             printf("Name not found\n");
             return e_fail;
         break;
-        case 2:
+        case '2':
             printf("What phone number would you like to search for:\n");
             scanf("%s\n", targetPhone);
 
@@ -65,7 +71,7 @@ Status search_contact(AddressBook* address_book) {
             printf("Phone number not found\n");
             return e_fail;
         break;
-        case 3:
+        case '3':
             printf("What phone number would you like to search for:\n");
             scanf("%s\n", targetEmail);
             
@@ -78,7 +84,7 @@ Status search_contact(AddressBook* address_book) {
             printf("Email address not found\n");
             return e_fail;
         break;
-        case 4:
+        case '4':
             printf("What serial number would you like to search for:\n");
             scanf("%s\n", targetSerial);
             
@@ -93,6 +99,6 @@ Status search_contact(AddressBook* address_book) {
         break;
         default:
         printf("Error: Please enter a valid option.\n");
-        goto SELECT_SEARCH;
+        search_contact(address_book);
     }
 }
